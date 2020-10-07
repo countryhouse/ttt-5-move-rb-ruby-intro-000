913f059a75a8bf24f1a8ca1d49922dcaa171b5df
May Geolot <meriel.may@gmail.com>
2020-10-07 17:45:05 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,8 +13,8 @@ def input_to_index(num)
   num.to_i - 1
 end
 
-def move(board, index, character = "X")
-  board[index] = character
+def move(array, index, character = "X")
+  array[index] = character
 end
 
 move(board, 0, "X")
